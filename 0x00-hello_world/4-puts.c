14d738eb3dba5af430b03655d15851b6cd92ac15
yeshan111 <yeshan111@gmail.com>
2022-06-02 04:44:01 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+/**
+ * main (funtion) - entry point
+ *
+ * return must always be 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
