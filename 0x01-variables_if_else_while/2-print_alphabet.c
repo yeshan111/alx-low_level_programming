35a79f3fe5e3e6cb3b00656e4d47e392c853239b
yeshan111 <yeshan111@gmail.com>
2022-06-03 05:43:18 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+	char c;
+
+	for(c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	return(0);
+}	
