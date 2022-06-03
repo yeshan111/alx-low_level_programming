1dedb1b5c17c673c34810da117514b6c511e8e1d
yeshan111 <yeshan111@gmail.com>
2022-06-03 06:11:06 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 97; a < 123; a++)
+	{
+		if (a != 101 && a != 113)
+		{
+			putchar(a);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
