1a1cb09917cbdedaaba9945fb40f2355acb0f6df
yeshan111 <yeshan111@gmail.com>
2022-06-03 06:35:23 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		printf("%d\n", i);
+		i = i + 1;
+	}
+	return (0);
+}
