7e29bedf6dd7be78f6485d377b60cf72674df4f4
yeshan111 <yeshan111@gmail.com>
2022-06-03 06:44:57 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i = i + 1;
+	}
+	putchar('\n');
+	return (0);
+}
