ccae83bf4e033ecce20603a420a057b6fd3ee258
yeshan111 <yeshan111@gmail.com>
2022-06-03 07:39:19 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c, d;
+
+	c = 48;
+	d = 48;
+
+	while (d < 58)
+	{
+		c = 48;
+		while (c < 58)
+		{
+			if (d != i && e < c)
+			{
+				putchar(d);
+				putchar(c);
+				if (c == 57 && d == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			c++;
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
