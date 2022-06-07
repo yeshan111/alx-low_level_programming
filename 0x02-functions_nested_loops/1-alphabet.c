30589723ec2907f99d2f89016547c02414f40d3e
yeshan111 <yeshan111@gmail.com>
2022-06-07 04:23:00 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- * print_aplhabet - Entry point
+ * print_alphabet - prints alphabet in lowercase
  *
- * Return: Always 0 (Success)
- */
+ **/
 void print_alphabet(void)
 {
 	char c;
