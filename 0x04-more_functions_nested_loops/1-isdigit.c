b908bc1f07a3c13f6c31c36785cc4e6549eec022
yeshan111 <yeshan111@gmail.com>
2022-06-09 06:31:08 -0700

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - function that checks for digits from 0-9
+ * @c: int type number
+ * Return: 1 if uppercase, else 0
+ */
+int _isupper(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+
+	return (0);
+}
