35555fa56ea0a394c64a0c0ae1a7f61d69d683f1
yeshan111 <yeshan111@gmail.com>
2022-06-09 10:57:02 -0700

    Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,20 +6,25 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, k;
 
 	if (size <= 0)
 	{
 		_putchar('\n');
-	}
-	for (i = 1; i < size; i++)
+	} else
 	{
-		for (j = 1; j = (size - 1); j--)
+
+		for (i = 1; i <= size; i++)
 		{
-			_putchar(" ");
+			for (j = size - i ; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k < i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar("#");
 	}
-	_putchar('\n');
-	return (0);
 }
