34178c9283398fa909269fd16587b8ac57e97794
yeshan111 <yeshan111@gmail.com>
2022-06-13 06:52:02 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps integers
+ * @a: takes integer
+ * @b: takes integer
+ */
+void swap_int(int *a, int *b)
+{
+	int x, y;
+
+	x = *a;
+	y = *b;
+	a = y;
+	b = x;
+}
