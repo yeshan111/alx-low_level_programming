84feb0a82beba4a8dcb7282713b123b2ec1a9240
yeshan111 <yeshan111@gmail.com>
2022-06-15 11:24:34 -0700

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words in a string
+ * @s: accepts string array
+ * Return: result
+ */
+char *cap_string(char *)
+{
+	int i = 0;
+
+	if (s[i] >= 97 && s[i] <= 122)
+	{
+		s[i] -= 32;
+	}
+	while (s[i] != '\0')
+	{
+		if (s[i] == ' ' || s[i] == '\n' || s[i] == '\t'
+		    || s[i] == ',' || s[i] == ';' || s[i] == '.'
+		    || s[i] == '!' || s[i] == '?' || s[i] == '"'
+		    || s[i] == '(' || s[i] == ')' || s[i] == '{'
+		    || s[i] == '}')
+		{
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+			{
+				s[i + 1] -= 32;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
