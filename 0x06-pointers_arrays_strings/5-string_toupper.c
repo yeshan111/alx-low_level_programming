362ed15f9f697e29433c4892cb10bdac1179f4d8
yeshan111 <yeshan111@gmail.com>
2022-06-15 11:15:42 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lower case letter to upper case
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] -= 32;
+		}
+	}
+	return (s);
+}
