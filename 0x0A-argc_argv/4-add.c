d7116a07709702b8895ac3e321a68304ecb5046a
yeshan111 <yeshan111@gmail.com>
2022-06-24 05:24:38 -0700

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - adds postive numbers
+ * @argc: argument counter
+ * @argv: vector array
+ * Return: 0
+ */
+int main(int argc, char argv[])
+{
+	int i, n, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (n = 0; argv[i][n]; n++)
+		{
+			if (argv[i][n] < '0' || argv[i][n] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
