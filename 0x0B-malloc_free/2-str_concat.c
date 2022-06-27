5dab530f69314291e67e2aadf5fd3ddc0ee89d8a
yeshan111 <yeshan111@gmail.com>
2022-06-27 04:01:01 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,24 +8,32 @@
  *
  * Return: pointer of an array of chars
  */
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-	char *strout;
-	unsigned int i, j;
+	char *a;
+	int i, j, c, d;
 
-	if (str == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
 		;
 
-	strout = (char *)malloc(sizeof(char) * (i + 1));
-
-	if (strout == NULL)
+	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
+	if (a == NULL)
 		return (NULL);
 
-	for (j = 0; j <= i; j++)
-		strout[j] = str[j];
+	for (c = 0, d = 0; c < (i + j + 1); c++)
+	{
+		if (c < i)
+			a[c] = s1[c];
+		else
+			a[c] = s2[d++];
+	}
 
-	return (strout);
+	return (a);
 }
