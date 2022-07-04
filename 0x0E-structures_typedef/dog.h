006bb8e17fe6067d0f34a693db4e2ef4479e2ec3
yeshan111 <yeshan111@gmail.com>
2022-07-04 04:43:41 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: description
+ */
+struct dog
+{
+	char *name, *owner;
+	float age;
+};
