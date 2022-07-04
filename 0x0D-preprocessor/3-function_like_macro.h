3ce74c698f3490ebacef1d3df72940fdd1cac5a0
yeshan111 <yeshan111@gmail.com>
2022-07-04 03:48:40 -0700

    task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef 3_FUNCTION_LIKE_MACRO_H
-#define 3_FUNCTION_LIKE_MACRO_H
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
 
-#define ABS(x) (x * -1)
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
 
 #endif
