e5d63ee60caf6551971df644f65cd8a9d662b338
yeshan111 <yeshan111@gmail.com>
2022-07-07 02:16:48 -0700

    Task 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - prints numbers
+ * @separator: string between strings
+ * @n: number of strings
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *s;
+	va_list strings;
+
+	if (seperator == NULL)
+	{
+		exit;
+	}
+	va_start(strings, n);
+		for (i = 0; i < n; i++)
+		{
+			s = va_arg(strings, char *);
+
+			if (s == NULL)
+				s = "(nil)";
+			printf("%s", s);
+			if (i < n - 1)
+			{
+				printf("%s", separator);
+			}
+		}
+	va_end(strings);
+	printf("\n");
+}
