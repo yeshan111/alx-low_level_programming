b234aeab864361a686d2829facb69b54765bb56a
yeshan111 <yeshan111@gmail.com>
2022-07-14 06:58:09 -0700

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists"
+/**
+ * free_list - frees list_t
+ * @head: pointer to head of list
+ * Return: 0
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		free(temp->str);
+		head = head->next;
+		free(temp);
+	}
+}
