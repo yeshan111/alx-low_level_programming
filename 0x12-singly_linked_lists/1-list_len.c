f245535fd0fe74a51cd53355359a3d19548fe738
yeshan111 <yeshan111@gmail.com>
2022-07-14 05:51:41 -0700

    Task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - returns the number of elements in list_t
+ * @h: pointer to list
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h);
+{
+	int i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
