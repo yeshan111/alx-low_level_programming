fcf47608b135b69c8c08f6915eab5fa0b6342465
yeshan111 <yeshan111@gmail.com>
2022-07-25 04:38:54 -0700

    Task 1 dammit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,6 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-
 	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
@@ -23,8 +22,9 @@ int create_file(const char *filename, char *text_content)
 			i++;
 		wr = write(file, text_content, i);
 		if (wr != i)
-			return;
+			return (-1);
 	}
+
 	close(file);
 	return (1);
 }
