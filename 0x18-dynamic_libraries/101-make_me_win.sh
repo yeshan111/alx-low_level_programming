059326a706b2dba3d20b33292fa9d15472d6c69c
yeshan111 <yeshan111@gmail.com>
2022-09-05 08:27:50 -0700

    win fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/winwin.so https://github.com/ermiasbahru/alx-low_level_programming/raw/master/0x18-dynamic_libraries/winwin.so
+wget -q -O /tmp/winwin.so https://github.com/yeshan111/alx-low_level_programming/blob/main/0x18-dynamic_libraries/winwin.so
 export LD_PRELOAD=/tmp/winwin.so
