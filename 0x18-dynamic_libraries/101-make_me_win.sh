7cbe6365906caff164ba932581578c35e1935b7c
yeshan111 <yeshan111@gmail.com>
2022-09-05 09:51:32 -0700

    rip

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/putshack.so https://github.com/yeshan111/alx-low_level_programming/blob/main/0x18-dynamic_libraries/putshack.so
-export LD_PRELOAD=/tmp/putshack.so
+wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
