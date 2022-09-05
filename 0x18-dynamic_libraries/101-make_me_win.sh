43269ea3d6d6b1e51dbacdf840652ca4ce1fcd15
yeshan111 <yeshan111@gmail.com>
2022-09-05 09:49:40 -0700

    test3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/winwin.so https://github.com/yeshan111/alx-low_level_programming/blob/main/0x18-dynamic_libraries/putshack.so
+wget -q -O /tmp/putshack.so https://github.com/yeshan111/alx-low_level_programming/blob/main/0x18-dynamic_libraries/putshack.so
 export LD_PRELOAD=/tmp/putshack.so
